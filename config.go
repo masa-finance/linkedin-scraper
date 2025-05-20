eb8dd91e74639afba2249418cfa7d85a725f21a3
teslashibe <34052452+teslashibe@users.noreply.github.com>
2025-05-20 12:13:05 -0700

    feat: Implement NewConfig and add ErrAuthMissing

--- config.go
+++ config.go
@@ -17,3 +17,25 @@ type Config struct {
 	// Add other headers from the cURL that might need to be configurable or are dynamic
 	// We'll start simple and add more configurability as needed.
 }
+
+// NewConfig creates a new Config struct.
+func NewConfig(auth AuthCredentials, userAgent ...string) (*Config, error) {
+	if auth.LiAtCookie == "" || auth.CSRFToken == "" {
+		return nil, ErrAuthMissing
+	}
+
+	cfg := &Config{
+		Auth: auth,
+	}
+
+	if len(userAgent) > 0 && userAgent[0] != "" {
+		cfg.UserAgent = userAgent[0]
+	} else {
+		cfg.UserAgent = DefaultUserAgent // Assumes DefaultUserAgent is defined in constants.go
+	}
+
+	// We will add more parameters like Referer, XLiPageInstance, XLiTrack later
+	// as they might be dynamic or require more thought on how they're set.
+
+	return cfg, nil
+}
--- errors.go
+++ errors.go
@@ -1,1 +1,5 @@
 package linkedinscraper
+
+import "errors"
+
+var ErrAuthMissing = errors.New("linkedinscraper: authentication credentials (li_at, csrf_token) are missing")
